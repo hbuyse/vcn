50e27b870cd20e28c2df4920958fe8cbff9fea3f
Henri Buyse <henri.buyse@gmail.com>
2019-02-27 02:15:17 +0100

    Change DJANGO_SETTINGS_MODULE to vcn.settings.heroku

--- vcn/wsgi.py
+++ vcn/wsgi.py
@@ -7,17 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcn.settings.dev")
-
-# application = get_wsgi_application()
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcn.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcn.settings.heroku")
 
 application = get_wsgi_application()
