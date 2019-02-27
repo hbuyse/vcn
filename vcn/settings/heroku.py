5c291778a5931b54b67876e7297f712e57824fae
Henri Buyse <henri.buyse@gmail.com>
2019-02-27 02:48:39 +0100

    Update heroku settings

--- vcn/settings/heroku.py
+++ vcn/settings/heroku.py
@@ -5,33 +5,22 @@ import os
 from vcn.settings.dev import *
 
 # Get debug variable 
-DEBUG = os.getenv('DEBUG', 'FALSE') == 'TRUE'
+DEBUG = os.getenv('DEBUG', 'TRUE') == 'TRUE'
 
 ALLOWED_HOSTS = ['*']
 
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'account.middleware.LocaleMiddleware',
-    'account.middleware.TimezoneMiddleware',
-]
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+MIDDLEWARE.insert(0, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-if DEBUG:
+if not DEBUG:
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL', "postgresql://localhost:5432"))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL', "sqlite:///db.sqlite3"))
 }
 
 # Static files (CSS, JavaScript, Images)
